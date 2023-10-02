1b09fa19eb33b8cb54fffda6ab7d9667bc5f7c09
Sam <samuelcornelisse@gmail.com>
2023-10-02 13:56:41 +0200

    Split config into separate files

---
+++ .config/nvim/plugin/diffview.lua
@@ -0,0 +1,17 @@
+vim.keymap.set('n', '<C-k>', function()
+  if next(require('diffview.lib').views) == nil then
+    local vcs = require('galaxyline.provider_vcs')
+    local branch_name = vcs.get_git_branch()
+    if branch_name ~= nil then
+      vim.cmd('DiffviewOpen')
+      require'barbar.api'.set_offset(35, 'Version Control')
+    end
+  else
+    vim.cmd('DiffviewClose')
+    if require('nvim-tree.view').is_visible() then
+      require'barbar.api'.set_offset(30, 'File Explorer')
+    else
+      require'barbar.api'.set_offset(0)
+    end
+  end
+end)
---
+++ .config/nvim/plugin/lsp.lua
@@ -0,0 +1,73 @@
+-- Add additional capabilities supported by nvim-cmp
+local lspconfig = require('lspconfig')
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'ghdl_ls', 'lua_ls' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.omnisharp.setup {
+  cmd = { '/home/otacon/.local/share/omnisharp/OmniSharp', '--languageserver', '--hostPID', tostring(vim.fn.getpid()) },
+  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", "project.json"),
+}
+-- lsp-config signs.
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      local opts = {buffer = true}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Displays hover information about the symbol under the cursor
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+    -- Jump to the definition
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+    -- Jump to declaration
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+    -- Lists all the implementations for the symbol under the cursor
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+    -- Jumps to the definition of the type symbol
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+    -- Lists all the references 
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+    -- Displays a function's signature information
+    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+    -- Renames all references to the symbol under the cursor
+    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+
+    -- Show diagnostics in a floating window
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+    -- Move to the previous diagnostic
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+    -- Move to the next diagnostic
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+  end
+})
+
